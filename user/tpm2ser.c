fca95badeb406696b1462cd10f4e691fd28736b4
Scott Franzyshen <sfranzyshen@gmail.com>
2015-02-21 16:30:14 -0800

    Update tpm2ser.c

--- user/tpm2ser.c
+++ user/tpm2ser.c
@@ -1,1 +1,12 @@
+#include "osapi.h"
+#include "ets_sys.h"
+#include "driver/uart.h"
 
+void ICACHE_FLASH_ATTR tpm2ser_strip(uint8_t * buffer, uint16_t length) {
+
+}
+
+
+void ICACHE_FLASH_ATTR tpm2ser_init() {
+
+}
