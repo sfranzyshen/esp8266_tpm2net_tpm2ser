d175fca3be1329ba45dacac26ae8ae02116e3f7d
Scott Franzyshen <sfranzyshen@gmail.com>
2015-02-21 16:42:04 -0800

    Update tpm2ser.h

--- user/tpm2ser.h
+++ user/tpm2ser.h
@@ -1,1 +1,10 @@
 
+#ifndef _TPM2SER_H
+#define _TPM2SER_H
+
+#include "ets_sys.h"
+
+void ICACHE_FLASH_ATTR tpm2ser_strip( uint8_t * buffer, uint16_t length );
+void ICACHE_FLASH_ATTR tpm2ser_init();
+
+#endif
